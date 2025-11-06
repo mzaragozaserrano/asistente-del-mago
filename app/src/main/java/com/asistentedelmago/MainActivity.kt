3b006733cb3588104d4b0b5f750f5254795c726c
mzaragoza <miguel.zaragozaserrano@plexus.es>
2025-11-06 21:49:08 +0100

    Allow content to draw behind system bars

--- app/src/main/java/com/asistentedelmago/MainActivity.kt
+++ app/src/main/java/com/asistentedelmago/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,6 +18,7 @@ import com.asistentedelmago.ui.theme.AsistenteDelMagoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             AsistenteDelMagoTheme {
                 Surface(
